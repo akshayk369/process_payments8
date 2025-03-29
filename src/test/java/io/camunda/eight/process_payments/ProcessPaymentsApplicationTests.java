7d7fa2fcb2af2d53424627add37d8fea413076ba
Akshay KAlore <er.akshaykalore@gmail.com>
2025-03-29 21:53:53 +0530

    initial commit

---
+++ src/test/java/io/camunda/eight/process_payments/ProcessPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.camunda.eight.process_payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessPaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
